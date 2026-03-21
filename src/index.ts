184b09c3148146386d2c69d2aa3bb7d6dc230dc0
Joseph Mearman <joseph@mearman.co.uk>
2026-03-21 10:13:36 +0000

    feat(lib): add barrel export for programmatic API
    Export all library modules from single entry point:
    - Schema types, validators, enums, labels
    - Converters (JSON ↔ Markdown)
    - validate(), stats()
    - Query and mutation functions
    - I/O utilities

---
+++ src/index.ts
@@ -0,0 +1,102 @@
+// Schema types, validators, enums, labels
+export {
+  // Types
+  type SysProMDocument,
+  type Node,
+  type Relationship,
+  type RelationshipType,
+  type NodeStatus,
+  type NodeType,
+  type Text,
+  type Option,
+  type Operation,
+  type Task,
+  type ExternalReference,
+  type ExternalReferenceRole,
+  type Metadata,
+  // Schemas with .is() type guards
+  sysproMDocument,
+  node,
+  relationship,
+  nodeType,
+  nodeStatus,
+  relationshipType,
+  text,
+  option,
+  operation,
+  task,
+  externalReference,
+  externalReferenceRole,
+  metadata,
+  // Labels
+  NODE_TYPE_LABELS,
+  NODE_LABEL_TO_TYPE,
+  RELATIONSHIP_TYPE_LABELS,
+  RELATIONSHIP_LABEL_TO_TYPE,
+  EXTERNAL_REFERENCE_ROLE_LABELS,
+  EXTERNAL_REFERENCE_LABEL_TO_ROLE,
+  NODE_STATUSES,
+  NODE_FILE_MAP,
+  // Schema generator
+  toJSONSchema,
+} from "./schema.js";
+
+// Converters: JSON to Markdown
+export {
+  jsonToMarkdownSingle,
+  jsonToMarkdownMultiDoc,
+  jsonToMarkdown,
+} from "./json-to-md.js";
+
+// Converters: Markdown to JSON
+export {
+  markdownSingleToJson,
+  markdownMultiDocToJson,
+  markdownToJson,
+} from "./md-to-json.js";
+
+// Validation
+export { validate, type ValidationResult } from "./validate.js";
+
+// Stats
+export { stats, type DocumentStats } from "./stats.js";
+
+// Query
+export {
+  queryNodes,
+  queryNode,
+  queryRelationships,
+  traceFromNode,
+  type NodeFilters,
+  type RelationshipFilters,
+  type NodeDetail,
+  type TraceNode,
+} from "./query.js";
+
+// Mutation
+export {
+  addNode,
+  removeNode,
+  updateNode,
+  addRelationship,
+  removeRelationship,
+  updateMetadata,
+  type RemoveResult,
+} from "./mutate.js";
+
+// Utilities
+export { canonicalise } from "./canonical-json.js";
+export {
+  textToString,
+  textToLines,
+  textToMarkdown,
+  markdownToText,
+} from "./text.js";
+
+// IO
+export {
+  loadDocument,
+  saveDocument,
+  type Format,
+  type LoadedDocument,
+} from "./io.js";
